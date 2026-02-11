3167c5444a877e36e017dc8a93421e471d0a448a
vitalijkarsin-blip <vitalijkarsin@gmail.com>
2026-02-11 16:10:25 +0500

    3

--- Methodist_test/app.js
+++ Methodist_test/app.js
@@ -1,4 +1,4 @@
-﻿приветconst form = document.getElementById('requestForm');
+﻿const form = document.getElementById('requestForm');
 const result = document.getElementById('result');
 
 const titleEl = document.getElementById('trainingTitle');
